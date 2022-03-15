dafd0138d8fdc8e5f6008a5a9913b58df430a955
manassehkim <mainasseh@gmail.com>
2022-03-15 07:50:42 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+/**
+ * main - print _putchar new line
+ * Return: Always 0 (Success)
+ */
+
+int main 
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+}
+
+
+
