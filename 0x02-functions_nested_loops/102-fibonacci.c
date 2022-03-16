ff91d66b6cf767df254f9e54d7461d22f508973e
manassehkim <mainasseh@gmail.com>
2022-03-16 01:16:10 -0700

    the nth value

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,7 +14,9 @@ int main(void)
 
 	for (counter = 1; counter <= (countto / 2); counter++)
 	{
-		printf("%li, %li, ", a, b);
+		printf("%li %li", a, b);
+		_putchar(',');
+		_putchar(' ');
 		a += b;
 		b += a;
 	}
